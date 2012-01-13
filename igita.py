2741a736561b48eb4b323a8df55b42aaf3e9ea50
Lucas G Gassert <lggassert@gmail.com>
2012-01-13 18:53:13 -0200

    "Organization"

--- igita.py
+++ igita.py
@@ -11,13 +11,38 @@ from igita.commands import commands
 from igita.history import init_history
 
 class Igita(cmd.Cmd):
+
+    # cmd.Cmd overwrites
+    
     prompt = "igita >> "
+    
+    def preloop(self):
+        self.shell = commands()
+    
+        self.FNULL = open('/dev/null', 'w')
+        if system('git', 'rev-parse --show-toplevel', stdout=self.FNULL, stderr=self.FNULL):
+            print "This does not seem to be a git repository"
+            quit()
+        
+        self.history = init_history(readline)
     
     def default(self, line):
         self.default(line)
 
     def emptyline(self):
         self.do_ls('')
+
+    def do_EOF(self, line):
+        print ''
+        self.do_quit(line)
+
+    # Commands and completes
+
+    def do_cd(self, line):
+        self.shell.cd(line)
+        
+    def do_echo(self, line):
+        self.shell.echo(line)
     
     def do_git(self, line):
         self.shell.git(line)
@@ -28,41 +53,21 @@ class Igita(cmd.Cmd):
             return git_cmds
         else:
             return [command for command in git_cmds if command.startswith(text) ]
-
-    def do_ls(self, line):
-        self.shell.ls(line)
-            
-    def do_cd(self, line):
-        self.shell.cd(line)
     
     def do_hist(self, line):
         self.shell.hist(line, self.history)
-        
-    def do_echo(self, line):
-        self.shell.echo(line)
-        
+
+    def do_ls(self, line):
+        self.shell.ls(line)            
+
     def do_python(self, line):
         self.shell.python(line, self)
-    
-    def do_quit(self, line):
-        self.shell.quit(self.history)
-    
+
     def do_q(self, line):
         self.do_quit(line)
-
-    def do_EOF(self, line):
-        print ''
-        self.do_quit(line)
-
-    def preloop(self):
-        self.shell = commands()
     
-        self.FNULL = open('/dev/null', 'w')
-        if system('git', 'rev-parse --show-toplevel', stdout=self.FNULL, stderr=self.FNULL):
-            print "This does not seem to be a git repository"
-            quit()
-        
-        self.history = init_history(readline)
+    def do_quit(self, line):
+        self.shell.quit(self.history)    
         
 if __name__ == '__main__':
     Igita().cmdloop()
